60be57f3d8521b7800caff6a0a0958dd129a4996
jcastanov <j.castanov@uniandes.edu.co>
2022-11-20 13:44:02 -0500

    first commit

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,276 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Register_failed_with_wrong_email_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Register_failed_with_wrong_email_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Register_failed_with_wrong_email_0_document_0_default.png",
+        "label": "Register failed with wrong email",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/setup",
+        "referenceUrl": "http://localhost:3001/ghost/#/setup",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 55
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Register_failed_with_wrong_email_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Register_failed_with_short_password_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Register_failed_with_short_password_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Register_failed_with_short_password_0_document_0_default.png",
+        "label": "Register failed with short password",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/setup",
+        "referenceUrl": "http://localhost:3001/ghost/#/setup",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 53
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Register_failed_with_short_password_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Register_failed_without_title_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Register_failed_without_title_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Register_failed_without_title_0_document_0_default.png",
+        "label": "Register failed without title",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/setup",
+        "referenceUrl": "http://localhost:3001/ghost/#/setup",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 55
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Register_failed_without_title_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Register_failed_without_name_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Register_failed_without_name_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Register_failed_without_name_0_document_0_default.png",
+        "label": "Register failed without name",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/setup",
+        "referenceUrl": "http://localhost:3001/ghost/#/setup",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 57
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Register_failed_without_name_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Register_successful_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Register_successful_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Register_successful_0_document_0_default.png",
+        "label": "Register successful",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/setup",
+        "referenceUrl": "http://localhost:3001/ghost/#/setup",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 58
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Register_successful_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Login_failed_without_email_and_password_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Login_failed_without_email_and_password_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Login_failed_without_email_and_password_0_document_0_default.png",
+        "label": "Login failed without email and password",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/signin",
+        "referenceUrl": "http://localhost:3001/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 54
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Login_failed_without_email_and_password_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Login_failed_without_email_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Login_failed_without_email_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Login_failed_without_email_0_document_0_default.png",
+        "label": "Login failed without email",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/signin",
+        "referenceUrl": "http://localhost:3001/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 55
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Login_failed_without_email_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Login_failed_without_password_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Login_failed_without_password_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Login_failed_without_password_0_document_0_default.png",
+        "label": "Login failed without password",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/signin",
+        "referenceUrl": "http://localhost:3001/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 58
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Login_failed_without_password_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Login_failed_with_invalid_email_format_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Login_failed_with_invalid_email_format_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Login_failed_with_invalid_email_format_0_document_0_default.png",
+        "label": "Login failed with invalid email format",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/signin",
+        "referenceUrl": "http://localhost:3001/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 58
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Login_failed_with_invalid_email_format_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Login_failed_with_non-existent_user_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20221120-134046\\backstop_default_Login_failed_with_non-existent_user_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Login_failed_with_non-existent_user_0_document_0_default.png",
+        "label": "Login failed with non-existent user",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 5.7,
+        "url": "http://localhost:2368/ghost/#/signin",
+        "referenceUrl": "http://localhost:3001/ghost/#/signin",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 9.529583333333333,
+          "misMatchPercentage": "9.53",
+          "analysisTime": 56
+        },
+        "diffImage": "..\\bitmaps_test\\20221120-134046\\failed_diff_backstop_default_Login_failed_with_non-existent_user_0_document_0_default.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
